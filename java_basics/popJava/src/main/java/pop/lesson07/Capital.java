ccab9e3bdb0906c21766313308a9403d1ad29fdf
Aleksandr Nokhrin <a.v.nokhrin@yandex.ru>
2025-09-14 11:43:27 +0500

    ПОП: Maven: Управление зависимостями
    - добавлена реализация метода

--- java_basics/popJava/src/main/java/pop/lesson07/Capital.java
+++ java_basics/popJava/src/main/java/pop/lesson07/Capital.java
@@ -1,5 +1,7 @@
 package pop.lesson07;
 
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * Задача - применить библиотеку `commons-lang3`, добавленную в проект по заданию урока 7
  */
@@ -10,6 +12,6 @@ public class Capital {
      * @return строка, в которой каждое слово начинается с буквы в верхнем регистре
      */
     public static String createCapitalizedString(String str) {
-        return new String();
+        return StringUtils.capitalize(str);
     }
 }
