0ffcf42c65a3cec64792d9e9803f2fd317517c44
Aleksandr Nokhrin <a.v.nokhrin@yandex.ru>
2025-11-01 08:34:28 +0500

    Применение полиморфизма
    - добавлено решение с применением полиморфизма
    - добавлены ответы на вопросы и вопросы

---
+++ java_basics/oopJava/src/main/java/DemoTyping/MainPoly.java
@@ -0,0 +1,13 @@
+package DemoTyping;
+
+public class MainPoly {
+    public static void main(String[] args) {
+        C c;
+
+        c = new C();
+        c.sayHello();
+
+        c = new D();
+        c.sayHello();
+    }
+}
