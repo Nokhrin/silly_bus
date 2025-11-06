60da28985506b8f1010f80a020c541c2571edd94
Aleksandr Nokhrin <a.v.nokhrin@yandex.ru>
2025-11-06 08:33:27 +0500

    Понимание типизации и полиморфизма в Java
    - добавлено описание работы виртуальных/невиртуальных методов
    - добавлено оглавление, документация разделена на тематические документы

--- java_basics/oopJava/src/main/java/DemoTyping/C.java
+++ java_basics/oopJava/src/main/java/DemoTyping/C.java
@@ -5,4 +5,9 @@ public class C {
         System.out.println("hello from C");
         System.out.flush();
     }
+
+    public static void sayStatic() {
+        System.out.println("STATIC hello from C");
+        System.out.flush();
+    }
 }
