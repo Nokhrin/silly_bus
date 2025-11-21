43bc66a3e4c78aadece7d1918ed100b2c5d9f67b
Aleksandr Nokhrin <NokhrinAV@utgard.cbr.ru>
2025-11-21 08:41:47 +0500

    Парсинг
    - добавлен пример реализации на регулярках

---
+++ java_basics/oopJava/src/main/java/ParsingDraft/Main.java
@@ -0,0 +1,33 @@
+package ParsingDraft;
+
+public class Main {
+    public static void main(String[] args) {
+        CommandParser parser = new CommandParser();
+        OperationService service = new OperationService();
+
+        String[] commands = {
+                "deposit 1000",
+                "withdraw 100",
+                "transfer 200 from 1 to 2",
+        };
+
+        for (String cmd : commands) {
+            try {
+                Operation op = parser.parse(cmd);
+                System.out.println("Распознана команда: " + op);
+                service.execute(op);
+            } catch (Exception e) {
+                System.err.println("Ошибка при обработке: " + cmd);
+            }
+        }
+
+        /*
+        Распознана команда: Операция{тип:Deposit,сумма:1000,источник:0,получатель:0}
+        Баланс: 1000
+        Распознана команда: Операция{тип:Withdraw,сумма:100,источник:0,получатель:0}
+        Распознана команда: Операция{тип:Transfer,сумма:200,источник:1,получатель:2}
+        Ошибка при обработке: withdraw 100
+        Ошибка при обработке: transfer 200 from 1 to 2
+         */
+    }
+}
---
+++ java_basics/oopJava/src/main/java/ParsingDraft/OperationType.java
@@ -0,0 +1,8 @@
+package ParsingDraft;
+
+/**
+ * Операция - как тип данных - дополнительная проверка / страховка
+ */
+public enum OperationType {
+    Deposit, Withdraw, Transfer
+}
