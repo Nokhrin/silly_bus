eba5c3f4ab38f4ad8d40a9dc59f489071ddb7bb1
Aleksandr Nokhrin <NokhrinAV@utgard.cbr.ru>
2025-11-27 08:21:22 +0500

    Калькулятор
    - считывание целого числа
    - применены record, generics, optional

--- java_basics/oopJava/src/main/java/Calculator/Main.java
+++ java_basics/oopJava/src/main/java/Calculator/Main.java
@@ -29,4 +29,12 @@ package Calculator;
  *     Result - класс-контейнер, содержит пару value:offset - значение и финальное смещение
  */
 public class Main {
+    public static void main(String[] args) {
+        
+        System.out.println("record demo");
+        ParseResult<Integer> result = new ParseResult<>(42, 5, 6);
+        System.out.println(result.value());   // 42
+        System.out.println(result.start());   // 5
+        System.out.println(result.end());     // 6
+    }
 }
