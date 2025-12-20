4c9015131a6ecfdcec9df6e5f4ee1ef764f66982
Aleksandr Nokhrin <a.v.nokhrin@yandex.ru>
2025-12-20 08:59:29 +0500

    Понятие - КС-грамматика булевых выражений
    - преобразование левой рекурсии к правой

--- oopJava/src/main/java/CalcParser/Main.java
+++ oopJava/src/main/java/CalcParser/Main.java
@@ -6,27 +6,17 @@ import static CalcParser.Parser.parseAddSubExpression;
 
 public class Main {
     public static void main(String[] args) {
-        Optional<ParseResult<Expression>> exp = parseAddSubExpression("9 / ( 6 - 3 * 3 )", 0);
+        Optional<ParseResult<Expression>> exp;
+        
+        exp = parseAddSubExpression("-1 + 2", 0);
+        System.out.println(exp.get().value().evaluate());
+        //
+        exp = parseAddSubExpression("--1 + 2", 0);
         System.out.println(exp);
-        // Optional[ParseResult[value=BinaryExpression[left=
-        // NumValue[value=9.0], op=DIV, 
-        //                              right=BinaryExpression[
-        //                                      left=NumValue[value=6.0], op=SUB, 
-        //                                                    right=BinaryExpression[
-        //                                                              left=NumValue[value=3.0], op=MUL, right=NumValue[value=3.0]]]], start=0, end=17]]
-        //                9.0               /
-        //                                                      6.0           -
-        //                                                                          3.0         *           3.0
-
-        System.out.println(exp.get().value().evaluate()); // -3.0
-
-
-        Optional<Expression> expr = Optional.of(new BinaryExpression(new NumValue(1.0), Parser.Operation.ADD, new NumValue(2.0)));
-        if (expr.isPresent()) {
-            System.out.println("значение выражения = " + expr.get().evaluate());
-        } else {
-            System.out.println("выражение отсутствует");
-        }
+        //
+        exp = parseAddSubExpression("--1 + +2", 0);
+        System.out.println(exp);
+        //
     }
 
 }
