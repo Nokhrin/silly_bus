ff8514923dd9f80525b1bddfb29fea0595e2dd59
Aleksandr Nokhrin <a.v.nokhrin@yandex.ru>
2026-02-22 12:10:08 +0500

    Лямбда-выражения

---
+++ oopJava/concepts/src/main/java/org/example/lambdas/AnonymAndLambda.java
@@ -0,0 +1,21 @@
+package org.example.lambdas;
+
+public class AnonymAndLambda {
+    public static void main(String[] args) {
+        // реализовать интерфейс
+        // создать анонимный класс
+        // вызвать метод
+        ISpeakable xAnon = new ISpeakable() {
+            public void say() {
+                System.out.println("анонимный класс");
+            }
+        };
+        xAnon.say();
+
+        // реализовать интерфейс
+        // создать лямбду
+        // вызвать метод
+        ISpeakable xLambda = () -> System.out.println("лямбда");
+        xLambda.say();
+    }
+}
