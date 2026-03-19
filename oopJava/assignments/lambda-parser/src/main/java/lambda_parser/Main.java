f69cb9cad70a75b078f8cd53960d55db5cccd4de
Aleksandr Nokhrin <a.v.nokhrin@yandex.ru>
2026-03-19 08:50:37 +0500

    Лямбда - указатель

--- oopJava/assignments/lambda-parser/src/main/java/lambda_parser/Main.java
+++ oopJava/assignments/lambda-parser/src/main/java/lambda_parser/Main.java
@@ -1,7 +1,17 @@
 package main.java.lambda_parser;
 
+import java.util.function.Consumer;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello lambda");
+        String name = "lambda";
+
+        Consumer<String> lambda = (n) -> System.out.println(n + ", " + name); 
+        
+        lambda.accept(("hello"));
+        
+        // name = "world";
+        // изменение значения захваченной переменной вызовет ошибку
+        // java: local variables referenced from a lambda expression must be final or effectively final
     }
 }
