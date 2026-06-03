e4fe15a4abd091c90dffb6134b6b13b23cbecb25
Aleksandr Nokhrin <a.v.nokhrin@yandex.ru>
2026-06-03 22:36:47 +0500

    feat(antlr-expr): добавлена точка входа Main и конфигурация сборки jar

---
+++ antlr-expr/src/main/java/com/nokhrin/expr/Main.java
@@ -0,0 +1,23 @@
+package com.nokhrin.expr;
+
+import java.util.Scanner;
+
+public class Main {
+    static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Calculator calculator=new Calculator();
+        System.out.println("Введите выражение (\\q для выхода)");
+        while (scanner.hasNextLine()){
+            String input = scanner.nextLine().trim();
+            if (input.isEmpty() || input.equals("\\q")){
+                break;
+            }
+            try {
+                Object result =calculator.parse(input);
+                System.out.println(result);
+            } catch (Exception e){
+                System.err.println("Ошибка вычисления: " + e);
+            }
+        }
+    }
+}
